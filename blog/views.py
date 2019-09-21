fbff2b96c8f2fe9da20f0677856bc9b6358358e5
OHANA <1138700280@qq.com>
2019-09-21 16:45:33 +0800

    增加文章统计功能

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
-from django.db.models import Q
+from datetime import date
+
+from django.core.cache import cache
+from django.db.models import Q, F
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 
@@ -130,7 +133,7 @@ class TagView(IndexView):
         return queryset.filter(tag__id=tag_id)
 
 
-class PostDetailView(DetailView, CommonViewMixin):
+class PostDetailView(CommonViewMixin, DetailView):
     # model = Post
     template_name = 'blog/detail.html'
     queryset = Post.latest_posts()
@@ -144,6 +147,41 @@ class PostDetailView(DetailView, CommonViewMixin):
     #         'comment_list': Comment.get_by_target(self.request.path)
     #     })
     #     return context
+
+    # def get(self, request, *args, **kwargs):
+    #     response = super().get(request, *args, **kwargs)
+    #     Post.objects.filter(pk=self.object.id).update(pv=F('pv')+1, uv=F('uv')+1)
+    #
+    #     # from django.db import connection
+    #     # print(connection.queries)
+    #     return response
+
+    def get(self, request, *args, **kwargs):
+        response = super().get(request, *args, **kwargs)
+        self.handle_visited()
+        return response
+
+    def handle_visited(self):
+        increase_pv = False
+        increase_uv = False
+        uid = self.request.uid
+        pv_key = 'pv:%s:%s' % (uid, self.request.path)
+        uv_key = 'uv:%s:%s:%s' % (uid, str(date.today()), self.request.path)
+
+        if not cache.get(pv_key):
+            increase_pv = True
+            cache.set(pv_key, 1, 1*60)
+
+        if not cache.get(uv_key):
+            increase_uv = True
+            cache.set(uv_key, 1, 24*60*60)  # 24小时有效
+
+        if increase_pv and increase_uv:
+            Post.objects.filter(pk=self.object.id).update(pv=F('pv')+1, uv=F('uv')+1)
+        elif increase_pv:
+            Post.objects.filter(pk=self.object.id).update(pv=F('pv')+1)
+        elif increase_uv:
+            Post.objects.filter(pk=self.object.id).update(uv=F('uv') + 1)
 
 
 class SearchView(IndexView):
